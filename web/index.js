d1521ec6d9822525d6afcefbb6970489bb072798
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-03-16 18:22:05 +0100

    aggiunte le lib e alcuni file

---
+++ web/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public')); // per renderizzare i css insieme agli ejs
+
+app.use(function timelog(res, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+app.get('/', (req, res) => {
+    res.render("homepage/home") //si renderizza i .ejs 
+});
+
+app.get('/login', (req, res) => {
+    res.render("login/login")
+});
+
+app.listen(port)
