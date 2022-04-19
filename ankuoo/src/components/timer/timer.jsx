f9834f739554906bd38d2af0c97f568b4000cfd3
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-04-19 23:15:52 +0200

    created modal for timer

--- ankuoo/src/components/timer/timer.jsx
+++ ankuoo/src/components/timer/timer.jsx
@@ -7,25 +7,36 @@ import plusBtn from './plus-button.png'
 
 function Timer() {
 
-  function prova() {
-    return 'ciao';
+  function createTimer() {
+    var modal = document.querySelector('#modal')
+    modal.classList.add('active')
   }
 
   return (
     <div className='timer'>
       <div className="timer-create">
-        <img src={plusBtn} />
+        <img src={plusBtn} onClick={createTimer}/>
+
+        <div id="modal" className="modal">
+          <div className="modal-content">
+            <p> Ciao mondo </p>
+          </div>
+        </div>
+
       </div>
 
       <div className="timer-row">
         timer row -------------------------- <br/>
-        timer row -------------------------- <br /> 
+        timer row -------------------------- <br/> 
         timer row -------------------------- <br/>
         timer row -------------------------- <br/>
         timer row -------------------------- <br/>
         timer row -------------------------- <br/>
         timer row -------------------------- <br/>
-
+        timer row -------------------------- <br/>
+        timer row -------------------------- <br/>
+        timer row -------------------------- <br/>
+        timer row -------------------------- <br/>
 
       </div>
     </div>
