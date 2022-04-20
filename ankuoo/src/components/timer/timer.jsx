e9b1ab624b681d0bd60a6ba74afd48c198ff9a19
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-04-20 17:55:36 +0200

    hour and minutes of modal now works

--- ankuoo/src/components/timer/timer.jsx
+++ ankuoo/src/components/timer/timer.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 import './timer.scss'
 
-import plusBtn from './plus-button.png'
-import closeModal from './cross.png'
+import plusBtn from './images/plus-button.png'
+import closeModal from './images/cross.png'
+import upBtn from './images/up.png'
+import downBtn from './images/down.png'
+import dotdot from './images/dotdot.png'
 
 function Timer() {
 
@@ -17,6 +20,44 @@ function Timer() {
     modal.classList.remove('active')
   }
 
+  const [hour, setHour] = useState('00')
+  const [minute, setMinute] = useState('00')
+
+  function upHour() {
+    var hours = parseInt(hour) + 1;
+    if (hours <= 9) hours = '0' + hours
+    setHour(hours)
+  }
+
+  function downHour() {
+    if (hour > 0) {
+      var hours = parseInt(hour) - 1;
+      if (hours <= 9) hours = '0' + hours
+      setHour(hours)
+    }
+  }
+
+  function upMinute() {
+    if (minute < 59) {
+      var minutes = parseInt(minute) + 1;
+      if (minutes <= 9) minutes = '0' + minutes
+      setMinute(minutes)
+    } else {
+      upHour()
+      setMinute('00')
+    }
+  }
+
+  function downMinute() {
+    if (minute > 0) {
+      var minutes = parseInt(minute) - 1;
+      if (minutes <= 9) minutes = '0' + minutes
+      setMinute(minutes)
+    } else {
+      setMinute('59')
+    }
+  }
+
   return (
     <div className='timer'>
       <div className="timer-create">
@@ -26,13 +67,30 @@ function Timer() {
           <div className="modal-content">
             <h2> Create timer </h2>
             <img src={closeModal} id='close-modal-timer' className='close-modal' onClick={closeModalTimer}/>
+
+            <div className="set-timer">
+              <div className="hour">
+                <img className="up" src={upBtn} onClick={upHour} />
+                <p> {hour} </p>
+                <img className="down" src={downBtn} onClick={downHour} />
+              </div>
+
+              <img className="dotdot" src={dotdot} />
+
+              <div className="minutes">
+                <img className="up" src={upBtn} onClick={upMinute} />
+                <p> {minute} </p>
+                <img className="down" src={downBtn} onClick={downMinute} />
+              </div>
+            </div>
+
             <button className='create-button'> Create </button>
           </div>
         </div>
 
       </div>
 
-      <div className="timer-row">
+      <div className="timer-rows">
         timer row -------------------------- <br/>
         timer row -------------------------- <br/> 
         timer row -------------------------- <br/>
